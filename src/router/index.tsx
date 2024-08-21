5f3c3544ab5f37e8b3b4555cf634f0f27476e6e2
change <1932583520@qq.com>
2024-08-21 19:40:06 +0800

    docs: update doc

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -17,20 +17,39 @@ const ROUTER_CREATERS: Record<HistoryType, Function> = {
 
 class ViteRouter {
   modules: ModuleType;
+  /**
+   * 初始化并且 load modules
+   * 更多信息查看 load 方法
+   * @param {Record<string,Function<Promise<{default:any}>>>} modules 
+   */
   constructor(modules?: ModuleType) {
     this.modules = {};
     if (modules) {
       this.load(modules)
     }
   }
+  /**
+   * 倒入并解析全量模块
+   * 配合 vite 的 import.meta.glob使用
+   * @param {Record<string,Function<Promise<{default:any}>>>} modules 
+   * @example
+   * const modules = import.meta.glob('./pages/index.tsx')
+   * viteRouter.load(modules)
+   */
   load(modules: ModuleType) {
     const result: ModuleType = {}
     Object.keys(modules).forEach(pathname => {
+      // 去除前缀和后缀，如果这里index.tsx 和 index.{js,jsx,ts}也可能存在，这里统一认为是：代码不规范，后面的index文件替换检索之前的index
       const key = pathname.replace(/.+pages\//, '').replace(/.(jsx|js|tsx|ts)$/, '')
       result[key] = modules[pathname]
     })
     this.modules = result;
   }
+  /**
+   * 类似 webpack 中的 import(), 加载模块
+   * @param {string} path 模块的关键位置
+   * @returns 模块 dynamic import 函数
+   */
   import(path: string) {
     // 如果 path 以 ./ 开头，去除 ./
     let key = path;
@@ -55,10 +74,24 @@ class ViteRouter {
     // 没有找到，抛出错误
     throw new Error(`can not found module named ${path}`)
   }
+  /**
+   * 懒加载模块
+   * @param {string} path 模块的关键位置
+   * @returns 查找模块，返回使用React.lazy包裹的结果
+   */
   lazy(path: string) {
     const module = this.import(path)
     return React.lazy(module);
   }
+  /**
+   * 创建react-router
+   * @param routes 配置的routes
+   * @param option 创建路由的参数
+   * @param option.history 路由的配置信息
+   * @param option.history.type 路由类型，可选 browser | hash | memory, 同react-router的创建类型
+   * @param option.base 同basename 路由的基础路径
+   * @returns router 返回 react-router 的 router 实例
+   */
   createRouter(routes: IRoute[], option?: CreateRouterOption) {
     const { history, base } = option || {};
     const { type = 'browser' } = history || {};
@@ -66,11 +99,7 @@ class ViteRouter {
     const router = creater(
       routes.map(route => {
         const { path, component, redirectTo } = route;
-        const config: Record<string, any> = {
-          path,
-          element: undefined,
-          Component: undefined
-        }
+        // 重定向，则不dynamic import 之间创建重定向组件
         if (redirectTo) {
           return {
             path,
@@ -81,12 +110,14 @@ class ViteRouter {
             )
           }
         }
+        // 存在component去dynamic import
         if (component) {
           return {
             path,
             Component: this.lazy(component)
           }
         }
+        // 不存在则创建一个碎片
         return {
           path,
           element: <Fragment />
--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,9 @@
 export interface IRoute {
-  path: string,
-  component?: string,
-  name?: string,
-  children?: IRoute[],
-  redirectTo?: string,
+  path: string, // 路由路径
+  component?: string, // 对应的页面组建所在位置
+  name?: string, // 名称, 暂时没用到
+  children?: IRoute[], // 子路由，暂时没用到
+  redirectTo?: string, // 重定向地址
 }
 
 export type ModuleType = Record<string, () => Promise<any>>;
